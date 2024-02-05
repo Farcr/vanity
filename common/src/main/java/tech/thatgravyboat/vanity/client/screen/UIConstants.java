5e1203d132b70a10b7f9d00db4bd64e8a462e905
Farcr <slender.farris@gmail.com>
2024-02-06 00:29:33 +0100

    Styling Table Art Tweaks

--- common/src/main/java/tech/thatgravyboat/vanity/client/screen/UIConstants.java
+++ common/src/main/java/tech/thatgravyboat/vanity/client/screen/UIConstants.java
@@ -2,8 +2,8 @@ package tech.thatgravyboat.vanity.client.screen;
 
 public class UIConstants {
 
-    public static final int TITLE_COLOR = 0x23211D;
+    public static final int TITLE_COLOR = 0x474238;
     public static final int INVENTORY_COLOR = 0x404040;
-    public static final int TITLE_Y = 34;
-    public static final int INVENTORY_Y = 122;
+    public static final int TITLE_Y = 32;
+    public static final int INVENTORY_Y = 127;
 }
