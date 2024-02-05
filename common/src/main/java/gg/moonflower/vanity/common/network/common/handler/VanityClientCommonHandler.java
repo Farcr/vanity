4e5cd4ddb079b667a15fa805752b61617b3ee37b
ThatGravyBoat <thatgravyboat@gmail.com>
2024-02-04 21:35:05 -0330

    Initial Commit

--- common/src/main/java/gg/moonflower/vanity/common/network/common/handler/VanityClientCommonHandler.java
+++
@@ -1,9 +0,0 @@
-package gg.moonflower.vanity.common.network.common.handler;
-
-import gg.moonflower.pollen.api.network.v1.packet.PollinatedPacketContext;
-import gg.moonflower.vanity.common.network.common.message.ClientboundConceptArtSyncPacket;
-
-public interface VanityClientCommonHandler {
-
-    void handleConceptArtSync(ClientboundConceptArtSyncPacket pkt, PollinatedPacketContext ctx);
-}
--- common/src/main/java/gg/moonflower/vanity/common/network/play/handler/VanityClientPlayHandler.java
+++
@@ -1,6 +0,0 @@
-package gg.moonflower.vanity.common.network.play.handler;
-
-import gg.moonflower.vanity.common.network.common.handler.VanityClientCommonHandler;
-
-public interface VanityClientPlayHandler extends VanityClientCommonHandler {
-}
