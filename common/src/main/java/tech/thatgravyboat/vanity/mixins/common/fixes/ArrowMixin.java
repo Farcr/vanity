941cbdaaed0fb5ceb051691e639e8e6aeb821040
ThatGravyBoat <thatgravyboat@gmail.com>
2024-02-10 14:20:04 -0330

    Fix arrows not retaining same itemstack on pick

--- common/src/main/java/tech/thatgravyboat/vanity/mixins/common/fixes/ArrowMixin.java
+++ common/src/main/java/tech/thatgravyboat/vanity/mixins/common/fixes/ArrowMixin.java
@@ -1,27 +1,38 @@
 package tech.thatgravyboat.vanity.mixins.common.fixes;
 
-import tech.thatgravyboat.vanity.common.util.EntityItemHolder;
 import net.minecraft.world.entity.projectile.Arrow;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import tech.thatgravyboat.vanity.common.util.EntityItemHolder;
 
 @Mixin(Arrow.class)
 public class ArrowMixin implements EntityItemHolder {
 
     @Unique
-    private ItemStack vanity$arrowItem = new ItemStack(Items.ARROW);
+    private ItemStack vanity$arrowItem = ItemStack.EMPTY;
 
     @Inject(
             method = "setEffectsFromItem",
             at = @At("HEAD")
     )
     private void vanity$setItem(ItemStack stack, CallbackInfo ci) {
-        this.vanity$arrowItem = stack;
+        this.vanity$arrowItem = stack.copyWithCount(1);
+    }
+
+    @Inject(
+            method = "getPickupItem",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    private void vanity$getItem(CallbackInfoReturnable<ItemStack> cir) {
+        if (this.vanity$arrowItem != null && !this.vanity$arrowItem.isEmpty()) {
+            cir.setReturnValue(this.vanity$arrowItem);
+        }
     }
 
     @Override
@@ -31,6 +42,6 @@ public class ArrowMixin implements EntityItemHolder {
 
     @Override
     public void vanity$setItem(ItemStack stack) {
-        this.vanity$arrowItem = stack;
+        this.vanity$arrowItem = stack.copyWithCount(1);
     }
 }
